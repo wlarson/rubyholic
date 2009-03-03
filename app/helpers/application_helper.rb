f6b028f0f75cb35c3f31264a908ce313d1f7d77a
Will Larson <wblarson@mac.com>
2009-03-03 15:56:30 -0800

    installed ym4r plugin & patch for rails 2.2.2.  added map_location method to application_helper.  display map when viewing location.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def map_location loc
+    @map = GMap.new("map_div")
+    @map.control_init(:large_map => true,:map_type => true)
+    @map.center_zoom_init([loc.latitude,loc.longitude],16)
+    @map.overlay_init(GMarker.new([loc.latitude,loc.longitude],
+#      :title => loc.name, :info_window => loc.description))
+      :title => loc.name)) 
+    @map
+  end
+
 end
